9be5fad5979019bfa4ec8f2c50758a4974da3a62
caseylholston <caseylholston@gmail.com>
2017-06-11 23:05:22 -0400

    Updated listen to PORT instead of number

--- server.js
+++ server.js
@@ -152,6 +152,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port 3000!");
 });
